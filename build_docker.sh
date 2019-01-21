4312e110d1f4050d27b882edd8e757bf1873ab75
caesar <caesar@kekxv.com>
2019-01-21 18:19:43 +0800

    更新build_docker

--- build_docker.sh
+++ build_docker.sh
@@ -2,4 +2,4 @@
 
 echo "sudo docker build -t raspi_zero_control ."
 
-echo "sudo docker run -name raspi_zero_control -it -v \`pwd\`/config.js:/app/config.js -e I2C_BUS=1 -e I2C_ADDRESS=36 -p 3000:3000 -p 3001:3001 raspi_zero_control npm start username=username password=password"
+echo "sudo docker run --name raspi_zero_control -it -v \`pwd\`/config.js:/app/config.js -e I2C_BUS=1 -e I2C_ADDRESS=36 -p 3000:3000 -p 3001:3001 raspi_zero_control npm start username=username password=password"
