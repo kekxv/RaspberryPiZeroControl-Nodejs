0e4598ca6246d372de3a6b62b0fa7c896113a9d5
caesar <caesar@kekxv.com>
2019-01-21 22:35:46 +0800

    更新说明

---
+++ README.md
@@ -0,0 +1,28 @@
+# 树莓派 zero 状态检测
+
+&nbsp;&nbsp;&nbsp;&nbsp;此项目是为了`树莓派 zero` 开发，用于查看树莓派状态，主要为 电池状态，实现低电量自动关机
+
+## 使用说明
+
+    npm install
+    # 初次使用时候可以设置帐号密码
+    npm start username=username password=password
+    
+## 运行界面
+
+> ![运行界面](./public/images/RaspberryStatus.png)
+    
+## 邮件通知 （需要自己搭建）
+
+> ![邮件通知](./public/images/RaspberryEmail.png)
+
+## 其他
+
+桌面 `ubuntu` 也能够跑起来
+
+> ![Ubuntu 运行界面](./public/images/UbuntuStatus.png)
+
+## 关于 `zero` 电池
+
+> ![关于 `zero` 电池](./public/images/RaspberryBattery.webp)
+> ![关于 `zero` 电池](./public/images/RaspberryBattery2.webp)
--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 const favicon = require('serve-favicon');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 require("./model/Tools");
 
 
-var app = express();
+let app = express();
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,45 +21,40 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.use(session({
-  resave: true, // don’t save session if unmodified
-  saveUninitialized: false, // don’t create session until something stored
-  secret: 'SessionID'
+    resave: true, // don’t save session if unmodified
+    saveUninitialized: false, // don’t create session until something stored
+    secret: 'SessionID'
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
-
 
 
 module.exports = app;
